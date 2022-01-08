d6c6a579a6f195fc71d7d00cbdddbb4d418698d5
DarkBlade307 <63086311+DarkBlade307@users.noreply.github.com>
2022-01-08 20:10:02 +1000

    realised that the file was named incorrectly

---
+++ CBs/NPCs/RedFret.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace CBs.NPCs
+{
+    public class RedFret : ModNPC
+    {
+
+        public float targetx = 0;
+        public float targety = 0;
+
+        public int vMax = 0;
+        public float vAccel = 0;
+
+        public float tVel = 0;
+        public float vMag = 0;
+
+        float someTimer = 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Red Fret");
+        }
+
+        public override void SetDefaults()
+        {
+            vMax = 6;
+            vAccel = 0.2f;
+            npc.width = 160;
+            npc.height = 160;
+            npc.damage = 15;
+            npc.defense = 15;
+            npc.aiStyle = 0;
+            npc.lifeMax = 400;
+            npc.HitSound = SoundID.DD2_SkeletonHurt;
+            npc.DeathSound = SoundID.DD2_SkeletonDeath;
+            npc.npcSlots = 5f;
+            npc.value = 12f;
+            npc.knockBackResist = 0f;
+            npc.boss = false;
+            npc.lavaImmune = true;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
+        }
+
+        public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
+        {
+            npc.lifeMax = 40000 + numPlayers * 40000;
+            npc.damage = 45;
+        }
+
+        public override void AI()
+        {
+            {
+                int bossWhoAmI = (int)npc.ai[1];
+                NPC boss = Main.npc[bossWhoAmI];
+                npc.Center = boss.Center + new Vector2(-320, -320).RotatedBy(someTimer);
+                someTimer += 0.05f;
+
+                
+            }
+            
+        }
+
+        public override bool CheckActive()
+        {
+            int bossWhoAmI = (int)npc.ai[1];
+            NPC boss = Main.npc[bossWhoAmI];
+            if (boss.active == false)
+            {
+                npc.active = false;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public RedFret()
+        {
+        }
+    }
+}
---
+++ CBs/Projectiles/BlueNote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace CBs.Projectiles
+{
+    public class BlueNote : ModProjectile
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Green Note");
+        }
+
+
+        public override void SetDefaults()
+        {
+            projectile.width = 32;
+            projectile.height = 32;
+
+            projectile.aiStyle = 29;
+            projectile.friendly = true;
+
+            projectile.penetrate = 4;
+
+            projectile.magic = true;
+
+
+        }
+    }
+}
