a6c47903ccd152aea35d94124be4f5aa17e9ea53
DarkBlade307 <63086311+DarkBlade307@users.noreply.github.com>
2022-01-08 19:46:33 +1000

    Add files via upload

---
+++ NPCs/BlueFret.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace CBs.NPCs
+{
+    public class BlueFret : ModNPC
+    {
+
+        public float targetx = 0;
+        public float targety = 0;
+
+        public int vMax = 0;
+        public float vAccel = 0;
+
+        public float tVel = 0;
+        public float vMag = 0;
+
+        float someTimer = 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Yello Fret");
+        }
+
+        public override void SetDefaults()
+        {
+            vMax = 6;
+            vAccel = 0.2f;
+            npc.width = 160;
+            npc.height = 160;
+            npc.damage = 20;
+            npc.defense = 20;
+            npc.aiStyle = 0;
+            npc.lifeMax = 600;
+            npc.HitSound = SoundID.DD2_SkeletonHurt;
+            npc.DeathSound = SoundID.DD2_SkeletonDeath;
+            npc.npcSlots = 5f;
+            npc.value = 12f;
+            npc.knockBackResist = 0f;
+            npc.boss = false;
+            npc.lavaImmune = true;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
+        }
+
+        public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
+        {
+            npc.lifeMax = 60000 + numPlayers * 60000;
+            npc.damage = 45;
+        }
+
+        public override void AI()
+        {
+            {
+                int bossWhoAmI = (int)npc.ai[1];
+                NPC boss = Main.npc[bossWhoAmI];
+                npc.Center = boss.Center + new Vector2(320, 320).RotatedBy(someTimer);
+                someTimer += 0.05f;
+            }
+        }
+
+        public override bool CheckActive()
+        {
+            int bossWhoAmI = (int)npc.ai[1];
+            NPC boss = Main.npc[bossWhoAmI];
+            if (boss.active == false)
+            {
+                npc.active = false;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public BlueFret()
+        {
+        }
+    }
+}
