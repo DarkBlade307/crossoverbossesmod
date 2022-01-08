02e45af3cd2073bb06c5d2f64d022bd26430cb66
Parwinner <parwinner2@gmail.com>
2022-01-08 22:30:32 +1000

    jtfjkf
    fhj

---
+++ CBs/Projectiles/CobaltSabot.cs
@@ -0,0 +1,26 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using CBs.NPCs.Bosses;
+namespace CBs.Projectiles
+{
+    public class CobaltSabot : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cobalt Sabot");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 30;
+            projectile.height = 7;
+            projectile.damage = 100;
+            projectile.knockBack = 20;
+            projectile.ranged = true;
+            //projectile.UseSound = SoundID
+
+            // Set other projectile.X values here
+        }
+    }
+}
